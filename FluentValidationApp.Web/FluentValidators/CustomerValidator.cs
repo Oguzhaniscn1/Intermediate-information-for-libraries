71d261d0c3d411a086876a38ba05e8ece51f33fd
Oğuzhan Işcan <oguzhaniscn28@gmail.com>
2023-11-25 04:55:48 +0300

    Add project files.

---
+++ FluentValidationApp.Web/FluentValidators/CustomerValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using FluentValidationApp.Web.Models;
+
+namespace FluentValidationApp.Web.FluentValidators
+{
+    public class CustomerValidator:AbstractValidator<Customer>//dto vs olabilir
+    {
+
+        public string NotEmptyMessage { get; } = "{PropertyName} alanı boş olamaz.";
+
+        public CustomerValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage(NotEmptyMessage);
+            RuleFor(x => x.Email).NotEmpty().WithMessage(NotEmptyMessage).EmailAddress().WithMessage("Email alanı Doğru formatta olmalıdır");
+            RuleFor(x => x.Age).NotEmpty().WithMessage(NotEmptyMessage).InclusiveBetween(18, 60).WithMessage("Age alanı 18 ile 60 arasında olmalıdır");
+            RuleFor(x => x.BirthDay).NotEmpty().WithMessage(NotEmptyMessage).Must(x=>
+            {
+                return DateTime.Now.AddYears(-18)>=x;
+            }).WithMessage("yaşınız 18 den büyük olmalıdır.");
+        }
+
+
+    }
+}
---
+++ FluentValidationApp.Web/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System.Security;
+
+namespace FluentValidationApp.Web.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+
+        public DateTime? BirthDay { get; set; }
+    }
+}
